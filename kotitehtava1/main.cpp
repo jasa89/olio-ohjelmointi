42acfecad5745a2f31c6a2dee259bce9e7debfe0
jasa89 <78540352+jasa89@users.noreply.github.com>
2024-11-05 16:06:57 +0200

    harjoitus 1 valmis.

--- kotitehtava1/main.cpp
+++ kotitehtava1/main.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 int main() {
 
-    int a, b;
-    cout << "Anna eka luku a: ";
+    int a, b; // määritetän muuttujat a ja b
+    cout << "Anna eka luku a: "; //pyydetään käyttäjältä ensimmäinen luku
     cin >> a;
-    cout << "Anna toinen luku b: ";
+    cout << "Anna toinen luku b: ";// pyydetään luku toinen luku
     cin >> b;
-    int summa = retSum(a, b);
-    cout << "Lukujen summa on: " << summa << endl;
-    float osamäärä = retDiv(a, b);
-    cout << "Lukujen osamaara on: " << osamäärä << endl;
-    calcSum(a, b);
-    calcDiv(a, b);
+    int summa = retSum(a, b);// lasketaan lukujen summa
+    cout << "Lukujen summa on: " << summa << endl;// tulostetaan lukujen summa
+    float osamäärä = retDiv(a, b);// lasketaan lukujen osamäärä
+    cout << "Lukujen osamaara on: " << osamäärä << endl;// tulostetaan lukujen osamäärä
+    calcSum(a, b);// kutsutaan calcsum funktiota
+    calcDiv(a, b);// kutsutaan calc div funktiota
 
     return 0;
 }
